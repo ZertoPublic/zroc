0b4d87fe302809fd83af6f986c179697a687e97c
Justin Paul <justin@jpaul.me>
2023-04-01 18:51:20 -0400

    Update getupdates.sh

--- cron/getupdates.sh
+++ cron/getupdates.sh
@@ -8,6 +8,8 @@ git pull
 
 # check if there are any changes
 if [[ $(git diff HEAD@{1} HEAD) ]]; then
+  # if getupdates.sh scripts is update it make it executable
+  chmod +x ./cron/getupdates.sh
   # if there are changes, run docker-compose up with force-restart
   docker-compose up -d --force-restart
 fi
